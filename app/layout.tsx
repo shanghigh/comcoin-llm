a114d74fe62c3903a04d0c137858c537cab88c35
shanghigh <95733088+shanghigh@users.noreply.github.com>
2025-01-28 15:00:47 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,12 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'COMCOIN'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  'A fully AI-powered answer engine with a generative UI for the COMCOIN Community.'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://morphic.sh'),
+  metadataBase: new URL('https:/comcoin-llm.vercel.app'),
   title,
   description,
   openGraph: {
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     title,
     description,
     card: 'summary_large_image',
-    creator: '@miiura'
+    creator: 'shanghigh'
   }
 }
 
